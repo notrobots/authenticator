2a5abb86584b8a949d68e58200e74cef04cc17ce
maxpilotto <maxpil99@outlook.com>
2022-10-06 11:06:04 +0200

    Add settings screen toolbar

--- app/src/main/java/dev/notrobots/authenticator/ui/settings/SettingsActivity.kt
+++ app/src/main/java/dev/notrobots/authenticator/ui/settings/SettingsActivity.kt
@@ -3,15 +3,20 @@ package dev.notrobots.authenticator.ui.settings
 import android.os.Bundle
 import dev.notrobots.androidstuff.activities.BaseActivity
 import dev.notrobots.androidstuff.extensions.viewBindings
+import dev.notrobots.authenticator.R
 import dev.notrobots.authenticator.databinding.ActivitySettingsBinding
 
 class SettingsActivity : BaseActivity() {
     private val binding: ActivitySettingsBinding by viewBindings()
+    private val toolbar by lazy {
+        binding.toolbarLayout.toolbar
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setContentView(binding.root)
 
         supportFragmentManager
             .beginTransaction()
