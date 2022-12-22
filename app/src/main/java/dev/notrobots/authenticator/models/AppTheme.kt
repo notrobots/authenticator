b57cc88f32494c65afe7adf32f570cb46ffd5446
maxpilotto <maxpil99@outlook.com>
2022-12-22 14:02:50 +0100

    Fix FollowSystem theme

--- app/src/main/java/dev/notrobots/authenticator/models/AppTheme.kt
+++ app/src/main/java/dev/notrobots/authenticator/models/AppTheme.kt
@@ -4,7 +4,7 @@ import androidx.annotation.StyleRes
 import dev.notrobots.authenticator.R
 
 enum class AppTheme(@StyleRes val id: Int) {
-    FollowSystem(0),
+    FollowSystem(R.style.AppThemeOverlay_BlueAzure),
     Light(R.style.AppThemeOverlay_BlueAzure),
     Dark(R.style.AppThemeOverlay_BlueAzure),
     PitchBlack(R.style.AppThemeOverlay_PitchBlack),
